3231aa11af9c8c956db4fea27563e19c14bd7453
zhangchao <zhangchaofromwf@126.com>
2024-10-11 21:37:36 +0800

    save

--- openGL_dae.cpp
+++ openGL_dae.cpp
@@ -4,7 +4,6 @@
 #ifdef PLATFORM_UNIX
 #include <unistd.h>
 #endif // PLATFORM_UNIX
-
 #include <iostream>
 
 /*
@@ -17,7 +16,10 @@ void processInput(GLFWwindow *window);
 float r = 1.0f,g = 0.0f,b = 1.0f;
 int main()
 {
-    glfwInit();
+    if (!glfwInit()) {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -32,22 +34,24 @@ int main()
     }
     glfwMakeContextCurrent(window);
 
-    // float vertices[] = {
-    // -0.5f, -0.5f, 0.0f,
-    //  0.5f, -0.5f, 0.0f,
-    //  0.0f,  0.5f, 0.0f
-    // };
 
-    // unsigned int VBO;
-    // glGenBuffers(1, &VBO);
-    // glBindBuffer(GL_ARRAY_BUFFER, VBO);  
-    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+    float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+    };
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);  
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     //glfwSetKeyCallback(window,) typedef void (* GLFWkeyfun)(GLFWwindow*,int,int,int,int);
