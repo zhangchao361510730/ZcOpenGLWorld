6fb15a71e4ecfd3b3fa0950ea46313218e1588a9
张超 <zhangchaofromwf@126.com>
2024-10-14 17:27:00 +0800

    save

--- src/singleTriangle.cpp
+++ src/singleTriangle.cpp
@@ -1,3 +1,4 @@
+#include <random>
 #include<iostream>
 
 #include"globaDefine.h"
@@ -7,18 +8,19 @@ extern const char* fragmentShaderSource;
 extern const char* vertexShaderSource; 
 
 
-singleTriangle::singleTriangle(/* args */)
-{
+singleTriangle::singleTriangle():gen(std::random_device{}()) {
+
 }
 
-singleTriangle::~singleTriangle()
-{
+singleTriangle::~singleTriangle() {
+
 }
 
 bool singleTriangle::init() {
+    
     if (!glfwInit()) {
         std::cout << "Failed to initialize GLFW" << std::endl;
-        return false;
+        return -1;
     }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -26,13 +28,13 @@ bool singleTriangle::init() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(windowsWidth, windowsHeight, "jojoOpenGl", NULL, NULL);
+    window = glfwCreateWindow(windowsWidth, windowsHeight, "jojoOpenGl", NULL, NULL);
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-    glfwSetFramebufferSizeCallback(window, singleTriangle::framebuffer_size_callback);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwMakeContextCurrent(window);
 
     // 这类似一个函数指针绑定，只不过还强制转化了一下
@@ -40,10 +42,116 @@ bool singleTriangle::init() {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+    glfwSetWindowUserPointer(window, (void*)this);
+
+    // build and compile our shader program
+    // ------------------------------------
+    // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512] = {0};
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        // 如果着色器语言编译错误,这里应该会显示
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // link shaders
+    //uint32_t
+    // typedef unsigned int __uint32_t;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);    // 顶点着色器装载程序
+    glAttachShader(shaderProgram, fragmentShader);  // 片段着色器装载程序
+    glLinkProgram(shaderProgram); // 链接着色器,有点像C++的程序链接
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);// 检查程序链接错误
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    // 链接完毕编译的着色器就没用了
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // 左边的-0.5, 0.5
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // left  
+         0.5f, -0.5f, 0.0f, // right 
+         0.0f,  0.5f, 0.0f  // top   
+    };
+
+    // 几个概念: 顶点对象,顶点缓冲区,顶点属性
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // 将顶点数据传递给 GPU。这一步将 vertices 数组中的数据复制到当前绑定的缓冲区（即 VBO）
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
+
+
     return true;    
 }
 
+bool singleTriangle::unInitResource() {
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+    glfwTerminate();
+}
+
 void singleTriangle::runDrawProcess() {
+    while(!glfwWindowShouldClose(window)) {
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(r, g, b, a);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw our first triangle
+        glUseProgram(shaderProgram);// 使用刚才开发好的着色器程序
+        glBindVertexArray(VAO); 
+        // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glBindVertexArray(0); // no need to unbind it every time 
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
     
 }
 
@@ -51,6 +159,13 @@ void singleTriangle::processInput(GLFWwindow *window) {
 
 }
 
-void  singleTriangle::framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 
+void  singleTriangle::framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    singleTriangle* thiz = (singleTriangle*)glfwGetWindowUserPointer(window);
+    glViewport(0, 0, width, height);
+    std::uniform_real_distribution<float> dis(0.0f, 1.0f); // 定义均匀分布范围
+    thiz->r = dis(thiz->gen);
+    thiz->b = dis(thiz->gen);
+    thiz->g = dis(thiz->gen);
+    thiz->a = dis(thiz->gen);
 }
--- src/singleTriangle.h
+++ src/singleTriangle.h
@@ -8,13 +8,18 @@ class singleTriangle
 {
 private:
     float r = 1.0f,g = 0.0f,b = 1.0f,a = 1.0f;
+    GLFWwindow* window = nullptr;
+    std::mt19937 gen; // 使用梅森旋转算法初始化随机数生成器
+    uint32_t shaderProgram;
+    uint32_t VBO;
+    uint32_t VAO;
 public:
     singleTriangle(/* args */);
     ~singleTriangle();
 
     bool init();
     void runDrawProcess();
-
+    bool unInitResource();
     void static framebuffer_size_callback(GLFWwindow* window, int width, int height);
     void processInput(GLFWwindow *window);
 };
