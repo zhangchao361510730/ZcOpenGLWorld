6e027c0c8e82f09c98478728efc644cfa3a58bc0
张超 <zhangchaofromwf@126.com>
2024-10-15 17:06:45 +0800

    save

--- src/GLLSTool/shaderLanguage.cpp
+++ src/GLLSTool/shaderLanguage.cpp
@@ -30,7 +30,7 @@ ShaderGLSLTool::ShaderGLSLTool(const char* vertexPath, const char* fragmentPath)
     const char* vShaderCode = vertexCode.c_str();
     const char * fShaderCode = fragmentCode.c_str();
     // 2. compile shaders
-    unsigned int vertex, fragment;
+    uint32_t vertex, fragment;
     // vertex shader
     vertex = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex, 1, &vShaderCode, NULL);
@@ -51,3 +51,22 @@ ShaderGLSLTool::ShaderGLSLTool(const char* vertexPath, const char* fragmentPath)
     glDeleteShader(vertex);
     glDeleteShader(fragment);
 }
+
+void ShaderGLSLTool::checkCompileErrors(unsigned int shader, std::string type) {
+    int success;
+    char infoLog[1024] = {0};
+    if (type != "PROGRAM") {
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+            std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+        }
+    } else {
+        glGetProgramiv(shader, GL_LINK_STATUS, &success);
+        if (!success) {
+            glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+            std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+        }
+    }
+    }
--- src/GLLSTool/shaderLanguage.h
+++ src/GLLSTool/shaderLanguage.h
@@ -17,52 +17,29 @@ public:
     ShaderGLSLTool(const char* vertexPath, const char* fragmentPath);
     // activate the shader
     // ------------------------------------------------------------------------
-    void use() 
+    inline void use() 
     { 
         glUseProgram(ID); 
     }
     // utility uniform functions
     // ------------------------------------------------------------------------
-    void setBool(const std::string &name, bool value) const
-    {         
+    inline void setBool(const std::string &name, bool value) const {         
         glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
     }
+    
     // ------------------------------------------------------------------------
-    void setInt(const std::string &name, int value) const
-    { 
+    inline void setInt(const std::string &name, int value) const { 
         glUniform1i(glGetUniformLocation(ID, name.c_str()), value); 
     }
+
     // ------------------------------------------------------------------------
-    void setFloat(const std::string &name, float value) const
-    { 
+    inline void setFloat(const std::string &name, float value) const { 
         glUniform1f(glGetUniformLocation(ID, name.c_str()), value); 
     }
 
 private:
     // utility function for checking shader compilation/linking errors.
     // ------------------------------------------------------------------------
-    void checkCompileErrors(unsigned int shader, std::string type)
-    {
-        int success;
-        char infoLog[1024];
-        if (type != "PROGRAM")
-        {
-            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-            if (!success)
-            {
-                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
-                std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
-            }
-        }
-        else
-        {
-            glGetProgramiv(shader, GL_LINK_STATUS, &success);
-            if (!success)
-            {
-                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
-                std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
-            }
-        }
-    }
+    void checkCompileErrors(unsigned int shader, std::string type);
 };
 #endif
