003a5d10b2ad6c8a5e9693d197628c312e6ee511
张超 <zhangchaofromwf@126.com>
2024-10-16 15:55:31 +0800

    save

--- src/baseInit.cpp
+++ src/baseInit.cpp
@@ -12,30 +12,39 @@ baseInit::~baseInit() {
 }
 
 bool baseInit::InitGlSource() {
-    if (!glfwInit()) {
-        std::cout << "Failed to initialize GLFW" << std::endl;
-        return false;
-    }
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    std::string path_fs = std::string(CMAKE_CURRENT_DIR).append("/glslFile/shader.fs");
-    std::string path_vs = std::string(CMAKE_CURRENT_DIR).append("/glslFile/shader.vs");
-    
-    window = glfwCreateWindow(windowsWidth, windowsHeight, "jojoOpenGl", NULL, NULL);
-    if (window == NULL) {
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    window = glfwCreateWindow(windowsWidth, windowsHeight, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
-        return false;
+        return -1;
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, baseInit::framebuffer_size_callback);
-    // 这类似一个函数指针绑定，只不过还强制转化了一下
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
-        return false;
+        return -1;
     }
+
+    std::string path_fs = std::string(CMAKE_CURRENT_DIR).append("/glslFile/shader.fs");
+    std::string path_vs = std::string(CMAKE_CURRENT_DIR).append("/glslFile/shader.vs");
+    
     glfwSetWindowUserPointer(window, (void*)this);
     shaderTool_ = new ShaderGLSLTool(path_vs.c_str(),path_fs.c_str());
     // vertex shader
--- src/singleTriangle.cpp
+++ src/singleTriangle.cpp
@@ -15,8 +15,6 @@ singleTriangle::~singleTriangle() {
 
 bool singleTriangle::InitGlSource() {
     baseInit::InitGlSource();
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // 左边的-0.5, 0.5
     float vertices[] = {
         // positions         // colors
          0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
@@ -24,7 +22,6 @@ bool singleTriangle::InitGlSource() {
          0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
     };
 
-    //unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
@@ -40,36 +37,19 @@ bool singleTriangle::InitGlSource() {
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    // glBindVertexArray(0);
-
     return true;    
 }
 
 void singleTriangle::runDrawProcess() {
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         // input
-        // -----
         processInput(window);
-
+        // render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
-        // render the triangle
         shaderTool_->use();
-    // 更新uniform颜色
-    float timeValue = glfwGetTime();
-    float greenValue = sin(timeValue) / 2.0f + 0.5f;
-    int vertexColorLocation = glGetUniformLocation(shaderTool_->attachId, "ourColor");
-    glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
