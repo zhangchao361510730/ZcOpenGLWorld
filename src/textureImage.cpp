cba3034e57ae41494734d171b61fb9f45c268c01
张超 <zhangchaofromwf@126.com>
2024-10-18 09:51:05 +0800

    save

--- src/textureImage.cpp
+++ src/textureImage.cpp
@@ -69,6 +69,8 @@ bool textureImage::InitGlSource() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
+    //stbi_set_flip_vertically_on_load(true);
+
     // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
     uint8_t *data = stbi_load(path_picture.c_str(), &width, &height, &nrChannels, 0);
     if (data) {
@@ -90,6 +92,8 @@ bool textureImage::InitGlSource() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
+ 
+
     data = stbi_load(path_picture2.c_str(), &width, &height, &nrChannels, 0);
     if (data) {
         // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
@@ -128,6 +132,7 @@ void textureImage::runDrawProcess() {
         // render container
         shaderTool_->use();
         glBindVertexArray(VAO);
+        
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
