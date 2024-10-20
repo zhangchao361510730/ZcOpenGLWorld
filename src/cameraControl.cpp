0fe5cc95c5b5226461918f607fe2506b93732a2c
zhangchao <zhangchaofromwf@126.com>
2024-10-20 19:43:48 +0800

    save

--- src/cameraControl.cpp
+++ src/cameraControl.cpp
@@ -8,3 +8,19 @@ cameraControl::cameraControl()
 cameraControl::~cameraControl()
 {
 }
+
+void cameraControl::framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+}
+
+void cameraControl::mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+}
+
+void cameraControl::scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+}
+
+void cameraControl::processInput(GLFWwindow* window)
+{
+}
--- src/cameraControl.h
+++ src/cameraControl.h
@@ -1,15 +1,23 @@
 #ifndef cameraControl_H_
 #define cameraControl_H_
-#include"baseInit.h"
 
-class cameraControl
-{
+#include"baseInit.h"
+#include"glm/glm.hpp"
+#include"glm/gtc/type_ptr.hpp"
+#include"glm/gtc/matrix_transform.hpp"
+
+class cameraControl:public baseInit {
 public:
 	cameraControl();
 	~cameraControl();
-
+	static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+	static void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+	static void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+	void processInput(GLFWwindow* window);
 private:
-
+	glm::vec3 cameraPos;
+	glm::vec3 cameraFront;
+	glm::vec3 cameraUp;
 };
 
 
