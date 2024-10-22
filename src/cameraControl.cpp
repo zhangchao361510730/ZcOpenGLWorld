d2ea44eb894f68435f34b18afd01280282b04429
张超 <zhangchaofromwf@126.com>
2024-10-22 16:49:28 +0800

    save

--- src/cameraControl.cpp
+++ src/cameraControl.cpp
@@ -213,7 +213,7 @@ void cameraControl::mouse_callback(GLFWwindow* window, double xposIn, double ypo
         这背后的数学原理参考 mathLearn/math_1.png 和 math_2.png
     */
     thiz->cameraFront = glm::normalize(front);
-    //std::cout<<"x is "<<front.x<<" y is "<<front.y<<" z is "<<front.z<<std::endl;
+    //std::cout<<"x is "<<front.x<<" y is "<<front.y<<" z is "<<front.z<<std::endl; aa
 }
 
 void cameraControl::scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
