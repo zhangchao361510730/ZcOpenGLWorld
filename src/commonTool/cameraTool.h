d0ae6e4c90efe0daca53d41c9ec11be2eb6e527a
张超 <zhangchaofromwf@126.com>
2024-10-23 14:28:42 +0800

    save

--- src/commonTool/cameraTool.h
+++ src/commonTool/cameraTool.h
@@ -23,6 +23,8 @@ const float ZOOM        =  45.0f;
 class cameraTool
 {
 private:
+    // calculates the front vector from the Camera's (updated) Euler Angles
+    void updateCameraVectors();
     /* data */
 public:
     // camera Attributes
@@ -41,18 +43,17 @@ public:
     // constructor with vectors
     cameraTool(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH);
     // constructor with scalar values
-    cameraTool(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
-    {
-        Position = glm::vec3(posX, posY, posZ);
-        WorldUp = glm::vec3(upX, upY, upZ);
-        Yaw = yaw;
-        Pitch = pitch;
-        updateCameraVectors();
-    }
+    cameraTool(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch);
     ~cameraTool();
 
-
-
+    // returns the view matrix calculated using Euler Angles and the LookAt Matrix
+    glm::mat4 GetViewMatrix();
+    // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+    void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+    // processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+    void ProcessMouseMovement(float xoffset = 0, float yoffset = 0, GLboolean constrainPitch = true);
+    // processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+    void ProcessMouseScroll(float yoffset);
 };
 
 
