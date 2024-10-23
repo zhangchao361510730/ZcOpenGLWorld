4fc0960e93a50303c6aae08e9aaa3eba994cbdc3
张超 <zhangchaofromwf@126.com>
2024-10-23 10:55:11 +0800

    save

---
+++ src/commonTool/caermaTool.h
@@ -0,0 +1,23 @@
+#ifndef cameraTool_H_
+#define cameraTool_H_
+
+class cameraTool
+{
+private:
+    /* data */
+public:
+    cameraTool(/* args */);
+    ~cameraTool();
+};
+
+cameraTool::cameraTool(/* args */)
+{
+}
+
+cameraTool::~cameraTool()
+{
+}
+
+
+
+#endif
