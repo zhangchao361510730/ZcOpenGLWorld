d1fd5617fb5407ef3963454f342729946af00dc2
张超 <zhangchaofromwf@126.com>
2024-10-25 17:04:09 +0800

    save

--- src/commonTool/animationTool.h
+++ src/commonTool/animationTool.h
@@ -1,6 +1,12 @@
 #ifndef animationTool_H_
 #define animationTool_H_
 
-
+#include <glm/glm.hpp>
+#include <map>
+#include <vector>
+#include <assimp/scene.h>
+#include <assimp/Importer.hpp>
+#include"commonTool/boneTool.h"
+#include"commonTool/assimpGlmHelpers.h"
 
 #endif
