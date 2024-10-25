7831e47e2044f8c3a0ca2062f7c96f2993475aaa
张超 <zhangchaofromwf@126.com>
2024-10-25 15:46:59 +0800

    save

---
+++ src/commonTool/assimpGlmHelpers.h
@@ -0,0 +1,28 @@
+#ifndef assimp_glm_helpers_H_
+#define assimp_glm_helpers_H_
+
+#include<assimp/quaternion.h>
+#include<assimp/vector3.h>
+#include<assimp/matrix4x4.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/quaternion.hpp>
+
+
+class AssimpGLMHelpers
+{
+public:
+	static glm::mat4 ConvertMatrixToGLMFormat(const aiMatrix4x4& from);
+
+	static inline glm::vec3 GetGLMVec(const aiVector3D& vec) 
+	{ 
+		return glm::vec3(vec.x, vec.y, vec.z); 
+	}
+
+	static inline glm::quat GetGLMQuat(const aiQuaternion& pOrientation)
+	{
+		return glm::quat(pOrientation.w, pOrientation.x, pOrientation.y, pOrientation.z);
+	}
+};
+
+
+#endif
