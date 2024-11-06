1ea49391d5b786872c0d5a5509c3b56bb9fcfebb
张超 <zhangchaofromwf@126.com>
2024-11-06 10:20:51 +0800

    save

--- src/cubeFunc/skyBox.cpp
+++ src/cubeFunc/skyBox.cpp
@@ -1,5 +1,7 @@
 #include"skyBox.h"
+#include"stb_image.h"
 #include<string>
+
 skyBox::skyBox(/* args */)
 {
 }
@@ -11,4 +13,64 @@ skyBox::~skyBox()
 bool skyBox::initSkyBox() {
     std::string SkyBoxPath_fs = std::string(CMAKE_CURRENT_DIR).append("/glslFile/skybox.fs");
     std::string SkyBoxPath_vs = std::string(CMAKE_CURRENT_DIR).append("/glslFile/skybox.vs");
+
+    std::vector<std::string> faces {
+        std::string(CMAKE_CURRENT_DIR).append("/pictureResource/skybox/right.jpg"),
+        std::string(CMAKE_CURRENT_DIR).append("/pictureResource/skybox/left.jpg"),
+        std::string(CMAKE_CURRENT_DIR).append("/pictureResource/skybox/top.jpg"),
+        std::string(CMAKE_CURRENT_DIR).append("/pictureResource/skybox/bottom.jpg"),
+        std::string(CMAKE_CURRENT_DIR).append("/pictureResource/skybox/front.jpg"),
+        std::string(CMAKE_CURRENT_DIR).append("/pictureResource/skybox/back.jpg")
+    };
+    cubemapTexture = loadCubemap(faces);
+    shaderSkyBox_ = new ShaderGLSLTool(SkyBoxPath_vs.c_str(),SkyBoxPath_fs.c_str());
+    return true;
+}
+
+void skyBox::runDrawProcess(glm::mat4 &view,glm::mat4 &projection) {
+    glDepthFunc(GL_LEQUAL); 
+    shaderSkyBox_->use();
+    view = glm::mat4(glm::mat3(camera_->GetViewMatrix())); // remove translation from the view matrix
+    shaderSkyBox_->setMat4("view", view);
+    shaderSkyBox_->setMat4("projection", projection);
+    glBindVertexArray(skyboxVAO);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    glBindVertexArray(0);
+    glDepthFunc(GL_LESS);
+}
+
+uint32_t skyBox::loadCubemap(std::vector<std::string> &faces) {
+    uint32_t textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+    int width, height, nrComponents;
+    for (uint32_t i = 0; i < faces.size(); i++)
+    {
+        unsigned char *data = stbi_load(faces[i].c_str(), &width, &height, &nrComponents, 0);
+        if (data) {
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            stbi_image_free(data);
+        } else {
+            std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+    
+    //  缩小过滤器 与  放大过滤器 都设置为线性过滤
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+/*
+GL_TEXTURE_CUBE_MAP：指明操作的是立方体贴图纹理。
+GL_TEXTURE_WRAP_S：表示设置的是 S轴方向的包裹方式，即纹理坐标的横向-纵向-深度-处理方式。
+GL_CLAMP_TO_EDGE：表示纹理坐标超出范围时，纹理会被 固定为边缘的像素值，避免出现拉伸或重复纹理的效果。通常用于避免纹理接缝的现象。
+*/
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    return textureID;
 }
--- src/cubeFunc/skyBox.h
+++ src/cubeFunc/skyBox.h
@@ -2,16 +2,24 @@
 #define H_skyBox_H
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <vector>
+#include"shaderLanguage.h"
+#include"cameraTool.h"
 
-
-class skyBox
-{
+class skyBox {
 private:
-    /* data */
+    ShaderGLSLTool*shaderSkyBox_;
+    cameraTool* camera_;
+    uint32_t skyboxVAO;
+    uint32_t cubemapTexture;
 public:
-    skyBox(/* args */);
+    skyBox();
     bool initSkyBox();
+    void runDrawProcess(glm::mat4 &view,glm::mat4 &projection);
+    uint32_t loadCubemap(std::vector<std::string> &faces);
     ~skyBox();
+
 };
+
 
 #endif
