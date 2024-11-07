7ba26579cb2d52d4a101c09c83e9b2ce7512183e
张超 <zhangchaofromwf@126.com>
2024-11-07 17:18:28 +0800

    save

--- openGLMain.cpp
+++ openGLMain.cpp
@@ -20,7 +20,7 @@ int main() {
 }
 
 /*
-书签记录，每次结束都更新下面的页数
+书签记录，每次结束都更新下面的页数 2
 https://learnopengl-cn.github.io/08%20Guest%20Articles/2020/01%20Skeletal%20Animation/#_1
 https://www.mixamo.com/#/?limit=48&page=1&type=Motion%2CMotionPack
 */
