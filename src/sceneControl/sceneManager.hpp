3cd6b8d2d08bce6e683f49d71977a7023c610a8f
张超 <zhangchaofromwf@126.com>
2024-11-08 10:51:51 +0800

    save

---
+++ src/sceneControl/sceneManager.hpp
@@ -0,0 +1,30 @@
+#include"scene.hpp"
+
+class SceneManager {
+private:
+    Scene* currentScene;
+
+public:
+    SceneManager() : currentScene(nullptr) {}
+
+    void ChangeScene(Scene* newScene) {
+        if (currentScene) {
+            currentScene->Cleanup();  // 清理当前场景
+            delete currentScene;
+        }
+        currentScene = newScene;
+        currentScene->Init();  // 初始化新场景
+    }
+
+    void Update(float dt) {
+        if (currentScene) {
+            currentScene->Update(dt);
+        }
+    }
+
+    void Render() {
+        if (currentScene) {
+            currentScene->Render();
+        }
+    }
+};
